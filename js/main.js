18441483ef462cc3f501f17f3f562d778bbf78b8
moretti-matteo <matteomoretti2000@hotmail.it>
2022-05-25 09:06:44 +0200

    implementata cancellazione messaggi

--- js/main.js
+++ js/main.js
@@ -6,6 +6,8 @@ const root = new Vue({
         currentUser: 0,
         msg: "",
         search: "",
+        emoji: false,
+        lastMsgSelected: null,
         contacts: [
             {
                 name: 'Michele',
@@ -15,17 +17,20 @@ const root = new Vue({
                     {
                         date: '10/01/2020 15:30:55',
                         message: 'Hai portato a spasso il cane?',
-                        status: 'sent'
+                        status: 'sent',
+                        modalActive: false
                     },
                     {
                         date: '10/01/2020 15:50:00',
                         message: 'Ricordati di stendere i panni',
-                        status: 'sent'
+                        status: 'sent',
+                        modalActive: false
                     },
                     {
                         date: '10/01/2020 16:15:22',
                         message: 'Tutto fatto!',
-                        status: 'received'
+                        status: 'received',
+                        modalActive: false
                     }
                 ],
             },
@@ -37,17 +42,20 @@ const root = new Vue({
                     {
                         date: '20/03/2020 16:30:00',
                         message: 'Ciao come stai?',
-                        status: 'sent'
+                        status: 'sent',
+                        modalActive: false
                     },
                     {
                         date: '20/03/2020 16:30:55',
                         message: 'Bene grazie! Stasera ci vediamo?',
-                        status: 'received'
+                        status: 'received',
+                        modalActive: false
                     },
                     {
                         date: '20/03/2020 16:35:00',
                         message: 'Mi piacerebbe ma devo andare a fare la spesa.',
-                        status: 'sent'
+                        status: 'sent',
+                        modalActive: false
                     }
                 ],
             },
@@ -59,17 +67,20 @@ const root = new Vue({
                     {
                         date: '28/03/2020 10:10:40',
                         message: 'La Marianna va in campagna',
-                        status: 'received'
+                        status: 'received',
+                        modalActive: false
                     },
                     {
                         date: '28/03/2020 10:20:10',
                         message: 'Sicuro di non aver sbagliato chat?',
-                        status: 'sent'
+                        status: 'sent',
+                        modalActive: false
                     },
                     {
                         date: '28/03/2020 16:15:22',
                         message: 'Ah scusa!',
-                        status: 'received'
+                        status: 'received',
+                        modalActive: false
                     }
                 ],
             },
@@ -81,12 +92,14 @@ const root = new Vue({
                     {
                         date: '10/01/2020 15:30:55',
                         message: 'Lo sai che ha aperto una nuova pizzeria?',
-                        status: 'sent'
+                        status: 'sent',
+                        modalActive: false
                     },
                     {
                         date: '10/01/2020 15:50:00',
                         message: 'Si, ma preferirei andare al cinema',
-                        status: 'received'
+                        status: 'received',
+                        modalActive: false
                     }
                 ],
             },
@@ -98,12 +111,14 @@ const root = new Vue({
                     {
                         date: '10/01/2020 15:30:55',
                         message: 'Ricordati di chiamare la nonna',
-                        status: 'sent'
+                        status: 'sent',
+                        modalActive: false
                     },
                     {
                         date: '10/01/2020 15:50:00',
                         message: 'Va bene, stasera la sento',
-                        status: 'received'
+                        status: 'received',
+                        modalActive: false
                     }
                 ],
             },
@@ -115,17 +130,20 @@ const root = new Vue({
                     {
                         date: '10/01/2020 15:30:55',
                         message: 'Ciao Claudia, hai novità?',
-                        status: 'sent'
+                        status: 'sent',
+                        modalActive: false
                     },
                     {
                         date: '10/01/2020 15:50:00',
                         message: 'Non ancora',
-                        status: 'received'
+                        status: 'received',
+                        modalActive: false
                     },
                     {
                         date: '10/01/2020 15:51:00',
                         message: 'Nessuna nuova, buona nuova',
-                        status: 'sent'
+                        status: 'sent',
+                        modalActive: false
                     }
                 ],
             },
@@ -137,12 +155,14 @@ const root = new Vue({
                     {
                         date: '10/01/2020 15:30:55',
                         message: 'Fai gli auguri a Martina che è il suo compleanno!',
-                        status: 'sent'
+                        status: 'sent',
+                        modalActive: false
                     },
                     {
                         date: '10/01/2020 15:50:00',
                         message: 'Grazie per avermelo ricordato, le scrivo subito!',
-                        status: 'received'
+                        status: 'received',
+                        modalActive: false
                     }
                 ],
             },
@@ -154,17 +174,20 @@ const root = new Vue({
                     {
                         date: '10/01/2020 15:30:55',
                         message: 'Ciao, andiamo a mangiare la pizza stasera?',
-                        status: 'received'
+                        status: 'received',
+                        modalActive: false
                     },
                     {
                         date: '10/01/2020 15:50:00',
                         message: 'No, l\'ho già mangiata ieri, ordiniamo sushi!',
-                        status: 'sent'
+                        status: 'sent',
+                        modalActive: false
                     },
                     {
                         date: '10/01/2020 15:51:00',
                         message: 'OK!!',
-                        status: 'received'
+                        status: 'received',
+                        modalActive: false
                     }
                 ],
             }
@@ -181,7 +204,7 @@ const root = new Vue({
         sendMsg() {
             const data = new Date().toLocaleString();
             console.log(data);
-            this.contacts[this.currentUser].messages.push({ date: data, message: this.msg, status: "sent" });
+            this.contacts[this.currentUser].messages.push({ date: data, message: this.msg, status: "sent", modalActive: false });
             this.msg = "";
             this.receiveMsg(data);
         },
@@ -191,7 +214,8 @@ const root = new Vue({
                     {
                         date: data,
                         message: "Ok!",
-                        status: "received"
+                        status: "received",
+                        modalActive: false
                     }
                 )
             }, 1000);
@@ -205,6 +229,22 @@ const root = new Vue({
                     this.contacts[contact].visible = true;
                 }
             }
+        },
+        deleteMsg(i) {
+            console.log("delete");
+            this.contacts[this.currentUser].messages.splice(i, 1);
+
+        },
+        showModal(i) {
+            console.log("showModal()");
+            if (this.lastMsgSelected !== null) {
+                this.lastMsgSelected.modalActive = !this.lastMsgSelected.modalActive;
+            }
+            this.lastMsgSelected = this.contacts[this.currentUser].messages[i];
+            this.contacts[this.currentUser].messages[i].modalActive = !this.contacts[this.currentUser].messages[i].modalActive;
+        },
+        showEmoji() {
+            this.emoji = !this.emoji;
         }
     }
 });
